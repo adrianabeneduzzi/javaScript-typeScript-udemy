52a9e9d750e7f59f0c96922f2ecca128b21c885d
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2022-10-18 11:38:16 -0300

    Aulas práticas

---
+++ secao2/comentarios.js
@@ -0,0 +1,14 @@
+// Escrever um comentário
+console.log('Bom diaa!') //Aqui tem outro comentário
+console.log('O sol já nasceu!') 
+console.log('Lá na fazendinha!') 
+
+/* 
+* comentário de bloco
+*loremmmmmm 
+*/
+
+console.log('Acordaaa!') 
+//console.log('Bom diaa!') 
+console.log('Bom diaa!') 
+//Comentário vai ser ignorado
---
+++ secao2/console-log.js
@@ -0,0 +1,8 @@
+//ponto e virgula é opcional - nas aulas será usado
+
+console.log('Adriana'); //String = Texto
+console.log("Adriana Beneduzzi"); //String
+console.log(`Adriana`);//Template Strings
+
+console.log(123456, 15.23 , 'Adriana');
+console.log(123456, 15.23); // Number = numero
