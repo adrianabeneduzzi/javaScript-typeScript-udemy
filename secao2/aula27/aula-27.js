07b67ace0cdd44328cc8d92601489aae6fc9c369
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2022-11-16 19:19:49 -0300

    Aulas práticas

---
+++ secao2/aula27/aula-27.js
@@ -0,0 +1,22 @@
+//let num1 = 9.50;
+//let num2 = Math.floor(num1); //Arredonda para baixo
+//let num2 = Math.ceil(num1); // Arredonda para cima
+//let num2 = Math.round(num1); 
+
+//console.log(Math.max(1,2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6)); // numero maior
+//console.log(Math.min(1,2, 3, 4, 5, -10, -50, 1500, 9, 8, 7, 6)); //numero menor
+//console.log(Math.random()); // gerar numeros aleatórios com varias casas decimais
+
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+//console.log(aleatorio);
+
+//console.log(Math.PI);
+
+//console.log(Math.pow(2, 10));
+//console.log(2 ** 10);
+
+let num1 = 9;
+console.log(num1 ** (1/2) );
+console.log(num1 ** 0.5);
+
+console.log(!!(100 / 0 )); // infinity
