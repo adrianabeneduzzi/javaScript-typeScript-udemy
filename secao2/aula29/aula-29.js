fe0cbf56783df218030a63703a7d7a6dacb5d98a
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2022-11-21 11:56:38 -0300

    Aula prática

---
+++ secao2/aula29/aula-29.js
@@ -0,0 +1,34 @@
+//const alunos = ['Luiz', 'Maria', 'João', 1, true, null];
+
+//const nome = 'Adriana Aparecida';
+//console.log(nome[0]);
+let alunos = ['Luiz', 'Maria', 'João'];
+alunos = 125;
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array); //para verificar se é array
+console.log(alunos);
+
+//alunos.push('Adriano');
+//alunos.push('Fátima');
+//console.log(alunos.slice(0,-2));
+//console.log(alunos[50]);
+//delete alunos[1]; // apaga o item, mas não exlui do array
+//console.log(alunos[1]);
+//const removido = alunos.shift(); //remove no inicio
+//console.log(removido);
+//console.log(alunos);
+//onst removido = alunos.pop();// remove do final
+//alunos.unshift('Victória'); //Adiciona no começo
+//alunos.unshift('Gabriela');
+//alunos.push('Gabriela'); //adiciona no fim
+//alunos.push('Adriana');
+//console.log(alunos.length);
+//alunos[alunos.length] = 'Ramoaldo'; //Adiciona no fim
+//alunos[alunos.length] = 'Almir';
+//alunos[alunos.length] = 'Victória';
+//alunos[0] = 'Eduardo'; //Altera
+//alunos[3] = 'Luiza'; // Adiciona
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[2]);
