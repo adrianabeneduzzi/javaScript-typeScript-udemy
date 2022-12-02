1f11bb861fe10d17c4f09c2684404821de8450a0
adrianabeneduzzi <adrianafbeneduzzi@gmail.com>
2022-12-02 13:01:06 -0300

    Aula prática

--- secao2/aula34/aula-34.js
+++ secao2/aula34/aula-34.js
@@ -1,3 +1,37 @@
-//alert(1)
+//IIFE
 
-const nome = 'Adriana';
+function meuEscopo () {
+    const form = document.querySelector('.form');
+
+    
+    /* Exemplo 1
+    form.onsubmit = function (evento) {
+        evento.preventDefault();
+        console.log('Foi enviado');
+    };*/
+
+     /* Com contador
+    let contador = 1;
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+        console.log(`Form nao foi enviado ${contador}`);
+        contador++;
+    }
+    */   
+   
+    function recebeEventoForm (evento) {
+        const nome = form.querySelector('.nome');
+        const nome = form.querySelector('.sobrenome');
+        const nome = form.querySelector('.peso');
+        const nome = form.querySelector('.altura');
+       
+
+    }
+
+   
+
+    form.addEventListener('submit', recebeEventoForm);
+
+}
+
+meuEscopo();
